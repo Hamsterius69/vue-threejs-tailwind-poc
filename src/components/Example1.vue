18be2423aafa32ac3873d84bf43f14d63988ec96
Ariel Aguilar <ariel@goja.com>
2022-08-31 15:39:14 -0400

    remove defaul page

---
+++ src/components/Example1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="example--box">
+    example 1
+  </div>
+</template>
+
+<script>
+// import Example1 from './Example1.vue';
+
+export default {
+  name: 'ManageExamples',
+  components: {
+    // Example1,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.example--box {
+    border: solid 2px black;
+    width: 500px;
+    height: 500px;
+}
+</style>
---
+++ src/components/ManageExamples.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-green-500">
+    <example-1/>
+  </div>
+</template>
+
+<script>
+import Example1 from './Example1.vue';
+
+export default {
+  name: 'ManageExamples',
+  components: {
+    Example1,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
